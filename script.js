aecc1765e1b0adb7e416e95fbc8baa15abd359f6
mesuara <mesuara@gmail.com>
2018-07-05 13:40:41 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let ex = document.getElementById('recy')
+
+
+
+
+window.addEventListener('scroll', () => {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    
+        ex.style.background = '#679436'
+         
+     }
+})
+
